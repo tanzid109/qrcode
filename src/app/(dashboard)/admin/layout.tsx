9b72512be1d0dd23750cb057afb332f0a452c9b5
tanzid109 <tanzid.unkhalil109@gmail.com>
2025-08-20 23:40:23 +0600

    header updated

--- src/app/(dashboard)/admin/layout.tsx
+++ src/app/(dashboard)/admin/layout.tsx
@@ -10,7 +10,7 @@ export default function AdminLayout({
     <main className="space-y-[22px]">
       <section className="sticky top-0 z-50 p-3 bg-transparent rounded-sm shadow-sm backdrop-blur-sm">
         <Headbar />
-      </section>
+      </section>  
       {children}
     </main>
   );
